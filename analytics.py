4976027616ffdf2f13a3d5d421a4abe7d3fcecd9
Avi Das <adas@colgate.edu>
2014-08-17 16:54:10 -0500

    Boilerplate methodviews for analytics api endpoints

--- analytics.py
+++ analytics.py
@@ -7,14 +7,34 @@ import json
 analytics_app = Blueprint('analytics_app', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
 class AnalyticsView(MethodView):
-	decorators = [login_required]
+    decorators = [login_required]
 
-	def get(self):
-		#Get reference of current user and send it to template
-		#Should only send relevant stats based on user
-		curr_user = User.objects.get_or_404(email=g.user.email)
-		return render_template('analytics/dash.html', user=curr_user)
+    def get(self):
+        #Get reference of current user and send it to template
+        #Should only send relevant stats based on user
+        curr_user = User.objects.get_or_404(email=g.user.email)
+        return render_template('analytics/dash.html', user=curr_user)
 
 
 analytics_app.add_url_rule('/', view_func=AnalyticsView.as_view('analytics'), methods=['GET'])
 
+
+class SkillAPI(MethodView):
+    decorators = [login_required]
+
+    def get(self, occupation_id=None):
+        pass
+
+
+class RelatedOccupationAPI(MethodView):
+    decorators = [login_required]
+
+    def get(self, occupation_id=None):
+        pass
+
+
+class OccupationLocationAPI(MethodView):
+    decorators = [login_required]
+
+    def get(self, occupation_id=None):
+        pass
